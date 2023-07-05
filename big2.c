ca9a1e0cada2a710671e10ec5fbc0b4f8d16d179
abc <ravi123@gmail.com>
2023-07-05 14:37:29 +0000

    add big2.c

---
+++ big2.c
@@ -0,0 +1,11 @@
+[200~#include <stdio.h>
+int main()
+{
+int num1, num2;
+printf("\nEnter the number 1 : ");
+scanf("%d",&num1);
+printf("\nEnter the number 2 : ");
+scanf("%d",&num2);
+printf("\nBiggest number : %d ", num1 > num2? num1:num2);
+return 0;
+}
