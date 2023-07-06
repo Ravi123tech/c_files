57fa504b6381f1a856c90814fa9ff4b61d1001df
abc <ravi123@gmail.com>
2023-07-06 11:21:28 +0000

    add new file and modifi

--- big2.c
+++ big2.c
@@ -1,11 +1,22 @@
-[200~#include <stdio.h>
-int main()
+#include <stdio.h>
+void biggest()
 {
-int num1, num2;
-printf("\nEnter the number 1 : ");
-scanf("%d",&num1);
-printf("\nEnter the number 2 : ");
-scanf("%d",&num2);
-printf("\nBiggest number : %d ", num1 > num2? num1:num2);
-return 0;
+    int num1, num2;
+    // Ask user to enter the two numbers
+    printf("Please Enter Two different values\n");
+    // Read two numbers from the user
+    scanf("%d %d", &num1, &num2);
+    if(num1 > num2)
+    {
+        printf("%d is Largest\n", num1);
+    }
+    else if (num2 > num1)
+    {
+        printf("%d is Largest\n", num2);
+    }
+    else
+    {
+        printf("Both are Equal\n");
+    }
+   // return 0;
 }
